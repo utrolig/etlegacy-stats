72076edb3abd6d8dbbd27ba9329195ae5e684773
Stian Bakken <sbakken@gmail.com>
2024-12-07 20:06:20 +0100

    Buggy buggesen

--- src/components/StatsTable.tsx
+++ src/components/StatsTable.tsx
@@ -1,26 +1,37 @@
 import { createMemo, createSignal, type Component } from "solid-js";
 import type { SortDirection, SortKey } from "../util/sorting";
 import { TeamTable } from "./TeamTable";
-import { type Stats } from "../util/stats";
+import { getMapStats, getMatchStats, type MatchStats } from "../util/stats";
+import type { GroupDetails } from "../util/stats-api";
 
 export type StatsTableProps = {
-  stats: Stats[];
+  matchDetails: GroupDetails;
+  stats: MatchStats;
 };
 
 export const StatsTable: Component<StatsTableProps> = (props) => {
   const [sortDir, setSortDir] = createSignal<SortDirection>("asc");
   const [sortKey, setSortKey] = createSignal<SortKey>("name");
 
+  const matchStats = createMemo(() => {
+    return getMatchStats(props.matchDetails);
+  });
+
+  const data = createMemo(() => {
+    return getMapStats("all", [], props.stats);
+    // return getMapStats("all", [], matchStats());
+  });
+
   return (
     <div class="flex flex-col gap-8 py-8">
       <TeamTable
-        stats={props.stats}
+        stats={data()}
         team="alpha"
         sortKey={sortKey()}
         sortDir={sortDir()}
       />
       <TeamTable
-        stats={props.stats}
+        stats={data()}
         team="beta"
         sortKey={sortKey()}
         sortDir={sortDir()}
