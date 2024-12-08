0e67c38e332572dfc31fef85157efbb2d165f66e
Stian Bakken <sbakken@gmail.com>
2024-12-08 12:33:47 +0100

    Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
+import cloudflare from "@astrojs/cloudflare";
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), solidJs()],
+  adapter: cloudflare(),
   output: "server",
 });
