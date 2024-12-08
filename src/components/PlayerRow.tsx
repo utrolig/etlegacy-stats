6faec130714e504a56a5136b8433f2eeeb5dc4a1
Stian Bakken <sbakken@gmail.com>
2024-12-08 18:33:35 +0100

    Chevronage

--- src/components/PlayerRow.tsx
+++ src/components/PlayerRow.tsx
@@ -1,4 +1,4 @@
-import { createMemo, For, type Component } from "solid-js";
+import { createMemo, createSignal, For, type Component } from "solid-js";
 import {
   getDeaths,
   getEfficiency,
@@ -12,20 +12,34 @@ import { getColoredNameParts } from "../util/colors";
 import clsx from "clsx";
 import { PlayerDetailedStats } from "./PlayerDetailedStats";
 import { Collapsible } from "@kobalte/core/collapsible";
+import { BsCaretRightFill } from "solid-icons/bs";
 
 export type PlayerRowProps = {
   stats: Stats;
 };
 
 export const PlayerRow: Component<PlayerRowProps> = (props) => {
+  const [open, setOpen] = createSignal(false);
+
   const kdr = createMemo(() => {
     return getKdr(props.stats);
   });
 
   return (
-    <Collapsible class="flex flex-col odd:bg-white/5">
+    <Collapsible
+      onOpenChange={setOpen}
+      open={open()}
+      class="flex flex-col odd:bg-white/5 hover:bg-white/10"
+    >
       <Collapsible.Trigger class="grid grid-cols-stats items-center gap-4  p-2 px-4">
-        <div class="flex justify-start pl-5">
+        <div class="flex items-center gap-1 justify-start pl-[2px]">
+          <BsCaretRightFill
+            class={clsx(
+              "text-mud-400 transition-transform",
+              open() && "rotate-90",
+            )}
+            size={12}
+          />
           <For each={getColoredNameParts(props.stats.name)}>
             {({ color, text }) => <span style={{ color }}>{text}</span>}
           </For>
