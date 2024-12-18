6f23af7aa5648daf73c316a360a0b2962e0b8819
Stian Bakken <sbakken@gmail.com>
2024-12-18 20:39:13 +0100

    Add cache purge endpoints

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,8 @@
 interface ImportMetaEnv {
   readonly API_TOKEN: string;
+  readonly CLOUDFLARE_API_KEY: string;
+  readonly CLOUDFLARE_EMAIL: string;
+  readonly CACHE_NUKE_TOKEN: string;
 }
 
 interface ImportMeta {
---
+++ src/pages/api/cache/list.ts
@@ -0,0 +1,40 @@
+import type { APIRoute } from "astro";
+
+export const POST: APIRoute = async ({ request }) => {
+  const { headers } = request;
+
+  const expectedHeader = `Bearer ${import.meta.env.CACHE_NUKE_TOKEN}`;
+  const authHeader = headers.get("Authorization");
+
+  if (authHeader !== expectedHeader) {
+    return new Response(JSON.stringify({ error: true, msg: "Invalid token." }));
+  }
+
+  const result = await fetch(
+    "https://api.cloudflare.com/client/v4/zones/c85e35e3765f5b50c46432221f2b3cfa/purge_cache",
+    {
+      method: "POST",
+      headers: {
+        "X-Auth-Email": import.meta.env.CLOUDFLARE_EMAIL,
+        "X-Auth-Key": import.meta.env.CLOUDFLARE_API_KEY,
+      },
+      body: JSON.stringify({ files: ["https://etlstats.stiba.lol"] }),
+    },
+  );
+
+  if (!result.ok) {
+    if (import.meta.env.DEV) {
+      console.error(result);
+      console.error(await result.json());
+    }
+    return new Response(JSON.stringify({ error: true }));
+  }
+
+  const data = await result.json();
+
+  if (import.meta.env.DEV) {
+    console.log({ data });
+  }
+
+  return new Response(JSON.stringify({ success: true }));
+};
