24f0bd59198c7b17b5dec080574aa85d02e99288
Stian Bakken <sbakken@gmail.com>
2025-04-13 19:33:41 +0200

    Fix things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
+import node from "@astrojs/node";
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), solidJs()],
-  adapter: cloudflare(),
+  adapter: node({
+    mode: "standalone",
+  }),
   output: "server",
 });
