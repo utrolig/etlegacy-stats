a4a5a09af552f369c5cf4f3ee51fb78a92da8f17
Stian Bakken <sbakken@gmail.com>
2026-03-16 20:01:45 +0100

    Up the cache size

--- varnish-cache/entrypoint.sh
+++ varnish-cache/entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 # Create in temp directory where varnish user has permissions
 envsubst < ./default.vcl.template > ./default.vcl
 
+# Allow cache size tuning from the runtime environment.
+: "${VARNISH_CACHE_SIZE:=1g}"
+
 # Start Varnish with standard parameters
-exec varnishd -F -f /tmp/varnish/default.vcl -a :80 -s malloc,256m -p ban_lurker_age=60
+exec varnishd -F -f /tmp/varnish/default.vcl -a :80 -s "malloc,${VARNISH_CACHE_SIZE}" -p ban_lurker_age=60
